f089a69dfc2d86a5a4c73f16659ce5478ed0459a
LEE LUNA <seojilee@student.42seoul.kr>
2026-01-24 18:51:04 +0900

    apply changed interface of iolib

--- srcs/write_bus.c
+++ srcs/write_bus.c
@@ -7,12 +7,16 @@ void write_bus(uint8_t cmd, uint8_t dcx) {
 	digital_write(LCD_RS, dcx); // dcs- command: low, index: high
 	pin_mode(LCD_RD, OUTPUT);
 	digital_write(LCD_RD, HIGH); // read inactivate
-	ddr_write(E, cmd & 0x2f);
-	port_write(E, cmd & 0x2f);
-	ddr_write(G, cmd & 0x10);
-	port_write(G, cmd & 0x10);
-	ddr_write(H, cmd & 0xc0);
-	port_write(H, cmd & 0xc0);
+//	for (digital_pins_t d = D0; d <= D7; ++d) {
+//		pin_mode(d, OUTPUT);
+//		digital_write(d, (cmd >> d) & 1);
+//	}
+	ddr_write(E, 0x2f, cmd & 0x2f);
+	port_write(E, 0x2f, cmd & 0x2f);
+	ddr_write(G, 0x10, cmd & 0x10);
+	port_write(G, 0x10, cmd & 0x10);
+	ddr_write(H, 0xc0, cmd & 0xc0);
+	port_write(H, 0xc0, cmd & 0xc0);
 	pin_mode(LCD_WR, OUTPUT);
 	digital_write(LCD_WR, LOW);
 	_delay_ms(1);
