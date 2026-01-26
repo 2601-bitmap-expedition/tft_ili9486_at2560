d9634dd8e1c796d7057c6ef99ff0c3df4a75ac8a
LEE LUNA <seojilee@student.42seoul.kr>
2026-01-26 17:17:42 +0900

    fix write_bus

--- srcs/write_bus.c
+++ srcs/write_bus.c
@@ -1,24 +1,26 @@
 #include "tft_ili9486_at2560.h"
 
 void write_bus(uint8_t cmd, uint8_t dcx) {
-	pin_mode(LCD_CS, OUTPUT);
 	digital_write(LCD_CS, LOW);
-	pin_mode(LCD_RS, OUTPUT);
 	digital_write(LCD_RS, dcx); // dcs- command: low, index: high
-	pin_mode(LCD_RD, OUTPUT);
 	digital_write(LCD_RD, HIGH); // read inactivate
-//	for (digital_pins_t d = D0; d <= D7; ++d) {
-//		pin_mode(d, OUTPUT);
-//		digital_write(d, (cmd >> d) & 1);
-//	}
-	ddr_write(E, 0x2f, cmd & 0x2f);
-	port_write(E, 0x2f, cmd & 0x2f);
-	ddr_write(G, 0x10, cmd & 0x10);
-	port_write(G, 0x10, cmd & 0x10);
-	ddr_write(H, 0xc0, cmd & 0xc0);
-	port_write(H, 0xc0, cmd & 0xc0);
-	pin_mode(LCD_WR, OUTPUT);
+	/*
+	 * D8 D9 D2 D3 D4 D5 D6 D7
+	 * H5 H6 E4 E5 G5 E3 H3 H4
+	 *
+	 * PORTE: 0011 1000, D2 << 2, D3 << 2, D5 >> 2
+	 * PORTG: 0010 0000, D4 << 1
+	 * PORTH: 0111 1000, D8 << 5, D9 << 5, D6 >> 3, D7 >> 3
+	 */
+	uint8_t converted_cmd = ((cmd & 0x0C) << 2) | ((cmd & 0x20) >> 2);
+	port_write(E, 0x38, converted_cmd);
+
+	converted_cmd = (cmd & 0x10) << 1;
+	port_write(G, 0x20, converted_cmd);
+
+	converted_cmd = ((cmd & 0x11) << 5) | ((cmd & 0xC0) >> 3);
+	port_write(H, 0x78, converted_cmd);
+
 	digital_write(LCD_WR, LOW);
-	_delay_ms(1);
 	digital_write(LCD_WR, HIGH);
 }
